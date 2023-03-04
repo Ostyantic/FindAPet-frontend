fa33f46ef9d4df5c7211d9d97cd19a3c2147631d
Anthony Lopez <Antl21292@gmail.com>
2023-03-04 18:36:30 -0500

    Added Auth0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={process.env.REACT_APP_DOMAIN}
+      clientId={process.env.REACT_APP_CLIENT}
+      authorizationParams={{redirect_uri: process.env.REACT_APP_REDIRECTURI}}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
