112e47623c559e52764ddc3fa03f3723ee977527
Araceligm90 <garcia.araceli90@gmail.com>
2023-03-04 20:51:28 -0500

    more styling done

--- src/App.js
+++ src/App.js
@@ -19,16 +19,6 @@ import {
 
 
 
-
-// Araceli´s code
-import "./App.css";
-import background from './background.jpg'
-import corporate from './Corporate.jpg'
-
-
-
-
-
 const containerStyling = {
   color: "white",
   backgroundColor: "DodgerBlue",
@@ -214,7 +204,7 @@ class App extends React.Component {
             />
           </Routes>
 
-          {/* <Container style={containerStyling}>
+           {/* <Container style={containerStyling}>
             <Accordion defaultActiveKey="type" alwaysOpen>
 
             {
@@ -414,7 +404,7 @@ class App extends React.Component {
             <Button onClick={() => this.handleSubmit()}>
               Search For Pets!
             </Button>
-          </Container>
+          </Container> */}
 
 
 
@@ -424,136 +414,7 @@ class App extends React.Component {
 
         </Router>
         
-        // Araceli´s work 
-class App extends React.Component {
-  render() {
-    return (
-      <body>
-      <div className="profile-area">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <div className="card">
-                <div className="img1"> <img src={background} alt="" /> </div>
-                <div className="img2"><img src={corporate} alt="" /> </div>
-                <div className="biography">
-                  <h2>Casey Glidewell</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-                <div className="socials">
-                  <i></i>
-                  <i></i>
-                  <i></i>
-                  <i></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="profile-area">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <div className="card">
-                <div className="img1"> <img src={background} alt="" /> </div>
-                <div className="img2"><img src={corporate} alt="" /> </div>
-                <div className="biography">
-                  <h2>Casey Glidewell</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-                <div className="socials">
-                  <i></i>
-                  <i></i>
-                  <i></i>
-                  <i></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="profile-area">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <div className="card">
-                <div className="img1"> <img src={background} alt="" /> </div>
-                <div className="img2"><img src={corporate} alt="" /> </div>
-                <div className="biography">
-                  <h2>Casey Glidewell</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-                <div className="socials">
-                  <i></i>
-                  <i></i>
-                  <i></i>
-                  <i></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      </body>
-      
-      
 
-      // <Container>
-      //   <Row>
-      //     <Col>
-      //       <Card className="card">
-      //         <Card.Img className="img1" src={background} alt="" />
-      //         <Card.Img className="img2" src={corporate} alt="" />
-      //         <Card.Body>
-      //           <Card.Title className="name">
-      //             Anthony Lopez
-      //           </Card.Title>
-      //           <Card.Text className="bio">
-      //             Here goes my biography.
-      //           </Card.Text>
-      //           <Card.Link></Card.Link>
-      //           <Card.Link></Card.Link>
-      //           <Card.Link></Card.Link>
-      //         </Card.Body>
-      //       </Card>
-      //     </Col>
-      //     <Col>
-      //       <Card>
-      //         <Card.Img className="img1" src="src/Susi1.JPG" alt="" />
-      //         <Card.Img className="img2" src="src/Corporate.jpg" alt="" />
-      //         <Card.Body>
-      //           <Card.Title className="name">
-      //             Casey Glidewell
-      //           </Card.Title>
-      //           <Card.Text className="bio">
-      //             Here goes my biography.
-      //           </Card.Text>
-      //           <Card.Link></Card.Link>
-      //           <Card.Link></Card.Link>
-      //           <Card.Link></Card.Link>
-      //         </Card.Body>
-      //       </Card>
-      //     </Col>
-      //     <Col>
-      //       <Card className="card">
-      //         <Card.Img className="img1" src="src/Susi1.JPG" alt="" />
-      //         <Card.Img className="img2" src="src/Corporate.jpg" alt="" />
-      //         <Card.Body>
-      //           <Card.Title className="name">
-      //             Araceli García
-      //           </Card.Title>
-      //           <Card.Text className="bio">
-      //             Here goes my biography.
-      //           </Card.Text>
-      //           <Card.Link></Card.Link>
-      //           <Card.Link></Card.Link>
-      //           <Card.Link></Card.Link>
-      //         </Card.Body>
-      //       </Card>
-      //     </Col>
-      //   </Row>
-      // </Container>
 
 
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,20 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import '../style/App.css'
+
 class Header extends React.Component {
   render() {
     return(
-      <Navbar sticky="top" expand='lg' bg="dark" variant="dark">
+      <Navbar className="header" expand='lg' variant="light">
         <Container>
           <Navbar.Brand href=''>Find A Pet!</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="headerNav">
-              <Nav.Link href=''>Home</Nav.Link>
-              <Nav.Link href=''>Profile</Nav.Link>
-              <Nav.Link href=''>AboutUs</Nav.Link>
+              <Nav.Link href='/'>Home</Nav.Link>
+              <Nav.Link href='/profile'>Profile</Nav.Link>
+              <Nav.Link href='/aboutus'>AboutUs</Nav.Link>
             </Nav>
           </Navbar.Collapse>
           <Button variant='success'>Log In</Button>
