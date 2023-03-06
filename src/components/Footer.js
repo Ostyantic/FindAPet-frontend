342770dbe8cdfb8b2f56879648440ebc47783987
Anthony Lopez <Antl21292@gmail.com>
2023-03-06 00:24:58 -0500

    fixed navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,13 @@ import Container from 'react-bootstrap/Container';
 class Footer extends React.Component {
   render() {
     return (
-      <Container>
-        <Navbar fixed="bottom" expand="lg" bg="dark" variant="dark">
+      // <Container>
+        <Navbar expand="xl" bg="dark" variant="dark">
           <Container>
             <Navbar.Brand style={{margin:'auto'}}>Anthony Lopez, Araceli Garcia &amp; Casey Glidewell 301 Final Project 2023 &copy;</Navbar.Brand>
           </Container>
         </Navbar>
-      </Container>
+      // </Container>
     );
   }
 }
