06bfc176f832ba5bbc2b8ed2ee6e5aa444632a9b
Anthony Lopez <Antl21292@gmail.com>
2023-03-07 01:39:59 -0500

    removed a little more

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+import { Auth0Provider} from "@auth0/auth0-react";
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
